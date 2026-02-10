21719952255d8aa58699c5cfa33eee601544d916
TARUN <ta>
2026-02-10 14:54:03 +0530

    Initial Commit

---
+++ MoviesHub/MoviesHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesHubApp.swift
+//  MoviesHub
+//
+//  Created by iMac02 on 10/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MoviesHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
