2a5758233a50276f18f0a0e7e90292dec2bfcd3f
TARUN <ta>
2026-02-16 16:40:28 +0530

    rename

--- MoviesHub/ContentView.swift
+++ MoviesHub/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 var MovieD = ["Star Wars": "Action", "Black Widow": "Thriller + Action", "Joker": "Action + Crime","Kalki": "Advanture + Drama", "South": "comedy", "Avatar": "Action + Adventure", "sutter Island": "Horror + Thriller", "24": "Action + comedy", "John Wick 2": "Action", "Golam": "Thriller", "Diesel": "Action + comedy", "RRR": "Documentary", "Lost City": "Documentary", "Oppenheimer": "Action + Crime","Madharaasi": "Drama"]
 
-var userName = ["Tarun", "Shubham", "Deepanshu"]
+var userName = ["Arun", "Shubham", "Deepanshu"]
 var imgName: [String] = ["Star Wars", "Black Widow", "Joker","Kalki", "South", "Avatar", "sutter Island", "24", "John Wick 2", "Golam", "Diesel", "RRR", "Lost City", "Oppenheimer","Madharaasi"]
 var movieCategory: [String] = ["Thriller", "Action", "Comedy", "Horror", "Documentary", "Fantasy", "Science Fiction", "Crime", "Drama"]
 struct ContentView: View {
